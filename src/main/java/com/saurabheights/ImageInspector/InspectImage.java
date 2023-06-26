c98b24078f91b2c66599c7053a80680c175e8d31
Saurabh Khanduja <saurabh.khanduja@ikara.ai>
2023-06-26 16:39:10 +0200

    Initial commit for image plugin.

---
+++ src/main/java/com/saurabheights/ImageInspector/InspectImage.java
@@ -0,0 +1,21 @@
+package com.saurabheights.ImageInspector;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import org.jetbrains.annotations.NotNull;
+
+public class InspectImage extends AnAction {
+    /**
+     * Performs the action logic.
+     * <p/>
+     * It is called on the UI thread with all data in the provided {@link DataContext} instance.
+     *
+     * @param e
+     * @see #beforeActionPerformedUpdate(AnActionEvent)
+     */
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
